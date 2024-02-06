030e4b31f4deb485e6bfeb56b119faa96db239e1
Anil prajapati <its.anilprajapatii@gmail.com>
2024-02-06 22:14:10 +0530

    Complexity Analysis and Array 17 April 2023 1D Arrays Questions

--- Complexity Analysis and Array 17 April 2023/1D Arrays  Assignment Questions/oneToFiveQ.java
+++ Complexity Analysis and Array 17 April 2023/1D Arrays  Assignment Questions/oneToFiveQ.java
@@ -5,6 +5,9 @@ public class oneToFiveQ {
         // Output 1: 16
         // Input 1: arr[] = {4,3,6,7,1}
         // Output 1: 11
+        System.out.println("-------------Q1----------------");
+
+
         int[] arr = {7,0,7,7,8};
         int result = 0;
         for (int i = 0; i < arr.length; i += 2) {
@@ -13,8 +16,25 @@ public class oneToFiveQ {
         System.out.println(result);
 
     }
+    public static void Q2(){
+        // Q2: Write a program to traverse over the elements of the array using for each loop and print all elements.
+        // Input 1: arr[] = {34,21,54,65,43}
+        // Input 1: arr[] = {4,3,6,7,1}
+        // Output 1: 34 54
+        // Output 1: 4 6
+
+        System.out.println("-------------Q2----------------");
+
+        int[] arr = {4,3,6,7,8};
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]%2==0){
+                System.out.println(arr[i]+" ");
+            }
+        }
+    }
 
     public static void main(String[] args) {
         Q1();
+        Q2();
     }
 }
