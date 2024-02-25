827eda366160c2a2c28baf687f491e6bc9c68e18
Anil prajapati <its.anilprajapatii@gmail.com>
2024-02-25 22:20:31 +0530

    Searching, Sorting and Bit Manipulation 24 April 2023 Sorting Array Assignment

--- Searching, Sorting and Bit Manipulation 24 April 2023/Sorting Array Assignment/oneToFiveQuestions.java
+++ Searching, Sorting and Bit Manipulation 24 April 2023/Sorting Array Assignment/oneToFiveQuestions.java
@@ -42,8 +42,49 @@ public class oneToFiveQuestions{
             System.out.print(value + " ");
         }
     }
+    public static void Q2(){
+
+        // Q2. WAP to sort an array in descending order using selection sort
+        // Output Array: {6, 5, 3, 1, 0}
+        // Input Array {3,5,1,6,0}
+
+        int[] array = {3, 5, 1, 6, 0};
+
+        System.out.println("Original Array:");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        int n = array.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int maxIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] > array[maxIndex]) {
+                    maxIndex = j;
+                }
+            }
+
+            // Swap the found maximum element with the element at index i
+            int temp = array[maxIndex];
+            array[maxIndex] = array[i];
+            array[i] = temp;
+        }
+
+        System.out.println("\nSorted Array in Descending Order:");
+        
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+    }
+
    public static void main(String[] args) {
         Q1();
+        Q2();
    }
     
 }
